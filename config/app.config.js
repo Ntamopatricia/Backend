17f5b9bb9040d42550c5c5ae91ba584be11df455
Ntamopatricia <133794663+Ntamopatricia@users.noreply.github.com>
2023-06-23 21:14:02 +0100

    edited_connection_string

--- config/app.config.js
+++ config/app.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config;
 
 const MONGO_DB_CONFIG = {
-    DB: 'mongodb+srv://suhdenzel:ftAD!.xsM_VB2BA@cluster0.tgr0x2n.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
+    DB: 'mongodb+srv://ntamopatricia098:Uv7JcJvPSSZNujzC@cluster0.ikocprd.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
 }
 
 module.exports = {
